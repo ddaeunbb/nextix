e48c89c9acbc862fc1b6601c989833b8d26a05d7
ddaeunbb <ddaeunbb@gmail.com>
2023-05-21 21:20:45 +0900

    [create]:loading-effect

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,7 +13,7 @@ export default async function Home({ searchParams }) {
   );
 
   if (!res.ok) {
-    throw new Error("Failed to fetch data"); // this will be caught by the error page and passed to the page as props
+    throw new Error("Failed to fetch data"); 
   }
 
   const data = await res.json();
