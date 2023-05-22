0a4124fd60bba9744978cee0b2ccc6334f35f309
ddaeunbb <ddaeunbb@gmail.com>
2023-05-22 13:47:44 +0900

    [add]:search-ability

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,21 @@
+"use client"
+import { useState } from 'react'
+import { useRouter } from 'next/navigation';
+
+export default function SearchBox() {
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    if (!search) return;
+    router.push(`/search/${search}`);
+  }
+  
+  return (
+    <form onSubmit={handleSubmit} className='flex max-w-6xl mx-auto justify-between items-center px-5 '>
+      <input value={search} onChange={(e)=> setSearch(e.target.value)} type="text" placeholder='Search keywords...' className='w-full h-14 rounded-sm placeholder-gray-500 outline-non bg-transparent flex-1'/>
+      <button type="submit" className='text-amber-600 disabled:text-gray-400' disabled={!search}>Search</button>
+    </form>
+  )
+}
